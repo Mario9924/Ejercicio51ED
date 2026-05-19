e41a8e677ff09d46cea4909c3bd4fe50d16abe49
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-05-19 23:24:16 +0200

    Revisión 2

--- src/ejercicio51ed/Ejercicio51ED.java
+++ src/ejercicio51ed/Ejercicio51ED.java
@@ -16,8 +16,9 @@ public class Ejercicio51ED {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        // Este el comentario número 1
+        // Este es el comentario número 1
         
+        // Este es el comentario número 2
     }
     
 }
